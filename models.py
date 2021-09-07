1371851642fdba5bfc97d1b70a613667d0aec0c9
dev-swd <tladnjsen@gmail.com>
2021-09-08 04:03:14 +0900

    First commit in account app

---
+++ models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class User(models.Model):
+    pass
+
+
+class Consumer(models.Model):
+    pass
+
+
+class Creator(models.Model):
+    pass
+
+
+class CertificationType(models.Model):
+    pass
+
+
+class AccountTypes(models.Model):
+    pass
+
+
+class PermissionType(models.Model):
+    pass
+
+
+class UserAccountType(models.Model):
+    pass
+
+
+class UserPermissionType(models.Model):
+    pass
