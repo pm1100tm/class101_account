193c1b33b719fce1c06a350e33e60f6649d08202
dev-swd <tladnjsen@gmail.com>
2022-02-13 17:05:39 +0900

    <Refactor:login>: 로그인 기능 수정 - User 모델에 last_login_date 필드 추가 - 유저가 로그인 했을 경우, login_count, last_login_date 만 업데이트 - 로그인 리턴 필드에 login_count 추가

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
+from django.utils  import timezone
 from django.db     import models
+
 from account.const import UserConst
 
 
@@ -24,9 +26,9 @@ class User(models.Model):
     id                 = models.BigAutoField(primary_key=True)
     email              = models.EmailField(max_length=30, unique=True, verbose_name='이메일')
     password           = models.CharField(max_length=80, null=True, verbose_name='비밀번호')
-    login_count        = models.IntegerField(default=0)
-    # last_login_date    = models.CharField(max_length=19)
-    is_deleted         = models.BooleanField(default=False)
+    login_count        = models.IntegerField(default=0, verbose_name='로그인 횟숫')
+    last_login_date    = models.DateTimeField(null=True, verbose_name='최신 로그인 날짜')
+    is_deleted         = models.BooleanField(default=False, verbose_name='탈퇴여부')
     created_at         = models.DateTimeField(auto_now=True, verbose_name='생성일')
     updated_at         = models.DateTimeField(auto_now_add=True, verbose_name='수정일')
     account_type       = models.ForeignKey('account.AccountTypes', on_delete=models.CASCADE, related_name='user')
